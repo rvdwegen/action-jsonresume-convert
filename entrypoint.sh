e7552d4f7981156413651d0dd50c8a33e532c608
Roel van der Wegen <github@aeternus.tech>
2022-10-01 21:33:15 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,8 @@
 
 THEME=${1}
 RESUME=${2}
-OUTPUT=${3}
+OUTPUT_HTML=${3}
+OUTPUT_PDF=${4}
 
 THEME_PACKAGE=jsonresume-theme-${THEME}
 echo "Installing theme: ${THEME}"
@@ -10,4 +11,5 @@ echo "Installing theme: ${THEME}"
 # NOTE: this needs to be installed locally, not globally
 npm install ${THEME_PACKAGE}
 
-resume export --resume ${RESUME} --theme ${THEME} --format html ${OUTPUT}
+resume export --resume ${RESUME} --theme ${THEME} --format html ${OUTPUT_HTML}
+resume export --resume ${RESUME} --theme ${THEME} --format pdf ${OUTPUT_PDF}
